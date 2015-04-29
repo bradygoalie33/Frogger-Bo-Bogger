595aca16b375025fa83751e9059e4b59560cefef
Brady Groharing <bradygoalie33@gmail.com>
2015-04-29 15:46:42 -0600

    Game?

---
+++ GameSolution/Game/main.cpp
@@ -0,0 +1,101 @@
+#include "core.h"
+
+int baseX = 0;
+int baseY = 2;
+
+struct Point
+{
+	float x;
+	float y;
+	Point(float x = 0.0f, float y = 0.0f) :
+		x(x), y(y) {}
+};
+
+Point operator+(const Point& left, const Point& right)
+{
+	return Point(left.x + right.x, left.y + right.y);
+}
+
+void drawLine(Core::Graphics& g, const Point& left, const Point& right)
+{
+	g.DrawLine(left.x, left.y, right.x, right.y);
+}
+struct Frog
+{
+	Point leftUpLegUpper;
+	Point leftUpLegLower;
+	Point leftDownLegUpper;
+	Point leftDownLegLower;
+	Point Body;
+	Point rightUpLegUpper;
+	Point rightUpLegLower;
+	Point rightDownLegUpper;
+	Point rightDownLegLower;
+
+	Point meBase;
+	Point velocity;
+
+	Frog() :
+		leftUpLegUpper(-50, 50),
+		leftUpLegLower(-50, 25),
+		leftDownLegUpper(-50,0),
+		leftDownLegLower(-50,-25),
+		rightUpLegUpper(0,50),
+		rightUpLegLower(0,25),
+		rightDownLegLower(0,0),
+		rightDownLegUpper(0,-25),
+		Body(-25, 0),
+		
+		meBase(400, 400)
+	{}
+		void drawTheyself(Core::Graphics& g)
+		{
+			
+			drawLine(g, meBase + leftUpLegUpper, meBase + leftUpLegLower);
+			drawLine(g, meBase + leftDownLegUpper, meBase + leftDownLegLower);
+			drawLine(g, meBase + rightUpLegUpper, meBase + rightUpLegLower);
+			drawLine(g, meBase + rightDownLegUpper, meBase + rightDownLegLower);
+			
+		}
+		void Intergrate()
+		{
+			meBase = meBase + velocity;
+		}
+};
+
+Frog frog;
+
+void checkKeyInput()
+{
+	float acceleration = 1;
+	if (Core::Input::IsPressed(Core::Input::KEY_UP))
+		frog.velocity.y -= acceleration;
+	if (Core::Input::IsPressed(Core::Input::KEY_DOWN))
+		frog.velocity.y += acceleration;
+	if (Core::Input::IsPressed(Core::Input::KEY_RIGHT))
+		frog.velocity.x += acceleration;
+	if (Core::Input::IsPressed(Core::Input::KEY_LEFT))
+		frog.velocity.x -= acceleration;
+}
+
+bool myUpdate(float dt)
+{
+	frog.Intergrate();
+	checkKeyInput();
+	return false;
+}
+
+void myDraw(Core::Graphics & graphics)
+{
+
+	frog.drawTheyself(graphics);
+}
+
+void main()
+{
+	Core::Init("My Game", 1200, 800);
+	Core::RegisterUpdateFn(myUpdate);
+	Core::RegisterDrawFn(myDraw);
+	Core::GameLoop();
+}
+
